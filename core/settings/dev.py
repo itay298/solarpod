3209ddc985404a8840e26b102c7c9464ced9fc80
ItayDavidov <davidovitay@gmail.com>
2026-04-06 20:35:33 +0300

    06/04/2026

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -2,4 +2,3 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['solarproject.local', '10.42.0.1', 'localhost', '127.0.0.1']
-SECURE_SSL_REDIRECT = True
--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,3 +1,3 @@
 from .base import *
 
-ALLOWED_HOSTS = ['solarproject.local', '10.42.0.1']
+ALLOWED_HOSTS = ['solarproject.local', '10.42.0.1', 'localhost', '127.0.0.1']
